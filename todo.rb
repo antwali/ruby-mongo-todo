cfe1442cc7a8b7fa570c5f9945fac4fe06f25699
antwali <98063586+antwali@users.noreply.github.com>
2022-06-20 15:14:42 -0500

    Second commit: added task IDs

--- todo.rb
+++ todo.rb
@@ -15,22 +15,37 @@ require "mongo"
 #Connects to the database
 client = Mongo::Client.new([ '127.0.0.1:27017' ], database: 'todo')
 
-#Prompts user to choose option between the 3 options
-
 choice = ARGV[0]
 case choice
 when 'help'
-    puts "Choose Option\nadd or show or delete"
-
+    puts "Choose Option\nadd or show or remove" #Prompts user to choose option between the 3 options
 when 'add'
-    client[:tasks].insert_one({ name: ARGV[1]}) #add fx
+
+count = client[:configs].find({key:"last_id"}).first["value"]
+count= count + 1
+client[:tasks].insert_one({ name: ARGV[1], task_id: count}) #add fx
+
+client[:configs].update_one({key: "last_id"},{'$set':{value:count}})
 
 when 'show'
-    found= client[:tasks].find({})
-    found.each {|x| puts x["name"] }#shows all documemnts in the collection
+    found= client[:tasks].find()
+    found.each {|x| puts "#{x["name"]} (#{x["task_id"]})" }#shows all documemnts in the collection
 
 when 'remove'
-    client[:tasks].delete_one({ name: ARGV[1]}) #remove
+    client[:tasks].delete_one({ task_id: ARGV[1].to_i}) #remove One
+
+# when 'removeAll'
+#     client[:tasks].delete_many({ name: ARGV[1]}) #remove All
+
 else
     puts "You can only choose 'help', 'add', 'show' or 'remove'"
 end
+
+
+##largest_id = collection.find().sort({task_id:-1}).limit(1) || 0
+
+## New config collection
+
+
+# client[:tasks].insert_one({ name: ARGV[1]}
+# puts count
